ba47533b381f1b366518a63444a7ae54eb1b891c
nesnoj <jonathan.amme@rl-institut.de>
2017-11-14 09:45:20 +0100

    add OEP API GET function

---
+++ windnode_abw/tools/data.py
@@ -0,0 +1,53 @@
+from windnode_abw.tools import config
+
+import requests
+import pandas as pd
+import logging
+logger = logging.getLogger('windnode_abw')
+
+
+def oep_get_data(schema, table, columns=[], conditions=[], order=''):
+    """Retrieve data from Open Energy Platform (OEP) / Database
+
+    Parameters
+    ----------
+    schema : :obj:`str`
+        Database schema
+    table : :obj:`str`
+        Database table
+    columns : :obj:`list` of :obj:`str`
+        Table columns
+    conditions : :obj:`list` of :obj:`str`
+        Conditions to be applied on query
+    order : :obj:`str`
+        Column which data is sorted by (ascending)
+    """
+
+    oep_url = config.get('data', 'oep_url')
+
+    if not schema or not table:
+        raise ValueError('Schema or table not specified.')
+
+    if columns:
+        columns = '&'.join('column='+col for col in columns)
+
+    if conditions:
+        conditions = '&' + '&'.join('where='+cond for cond in conditions)
+
+    if order:
+        order = '&order_by=' + order
+
+    url = oep_url +\
+          '/api/v0/schema/' +\
+          schema +\
+          '/tables/' +\
+          table +\
+          '/rows/?' + \
+          columns +\
+          conditions +\
+          order
+
+    result = requests.get(url)
+    logger.info('Response from OEP: ' + str(result.status_code))
+
+    return pd.DataFrame(result.json())
