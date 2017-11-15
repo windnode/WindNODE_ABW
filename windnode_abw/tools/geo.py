d2fe6797c603c2aa9babccd9137ac178e2684aef
nesnoj <jonathan.amme@rl-institut.de>
2017-11-15 10:27:15 +0100

    rm unused stuff

--- windnode_abw/tools/geo.py
+++ windnode_abw/tools/geo.py
@@ -2,11 +2,6 @@ import pyproj
 from functools import partial
 from geopy.distance import vincenty
 
-import os
-if not 'READTHEDOCS' in os.environ:
-    from shapely.geometry import LineString
-    from shapely.ops import transform
-
 import logging
 logger = logging.getLogger('edisgo')
 
@@ -53,47 +48,3 @@ def proj2conformal(network):
                    pyproj.Proj(init='epsg:{}'
                                .format(str(srid)))  # destination coordinate system
                    )
-
-
-def calc_geo_dist_vincenty(network, node_source, node_target):
-    """Calculates the geodesic distance between node_source and node_target
-    incorporating the detour factor in config.
-
-    Parameters
-    ----------
-    network : :class:`~.grid.network.Network`
-        The eDisGo container object
-    node_source : :class:`~.grid.components.Component`
-        Node to connect (e.g. :class:`~.grid.components.Generator`)
-    node_target : :class:`~.grid.components.Component`
-        Target node (e.g. :class:`~.grid.components.BranchTee`)
-
-    Returns
-    -------
-    :obj:`float`
-        Distance in m
-
-    Notes
-    -----
-    Adapted from `Ding0 <https://github.com/openego/ding0/blob/\
-        21a52048f84ec341fe54e0204ac62228a9e8a32a/\
-        ding0/tools/geo.py#L84>`_.
-    """
-
-    branch_detour_factor = network.config['connect']['branch_detour_factor']
-
-    # notice: vincenty takes (lat,lon)
-    branch_length = branch_detour_factor * vincenty((node_source.geom.y, node_source.geom.x),
-                                                    (node_target.geom.y, node_target.geom.x)).m
-
-    # ========= BUG: LINE LENGTH=0 WHEN CONNECTING GENERATORS ===========
-    # When importing generators, the geom_new field is used as position. If it is empty, EnergyMap's geom
-    # is used and so there are a couple of generators at the same position => length of interconnecting
-    # line is 0. See issue #76
-    if branch_length == 0:
-        branch_length = 1
-        logger.debug('Geo distance is zero, check objects\' positions. '
-                     'Distance is set to 1m')
-    # ===================================================================
-
-    return branch_length
