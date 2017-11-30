0d5329aa0a1ad75f01ba8b147039fe609decc6a0
nesnoj <jonathan.amme@rl-institut.de>
2017-11-30 11:06:32 +0100

    add excel reader for oemof nodes

--- windnode_abw/tools/data.py
+++ windnode_abw/tools/data.py
@@ -1,4 +1,5 @@
 from windnode_abw.tools import config
+import os
 import requests
 import pandas as pd
 import logging
@@ -62,3 +63,40 @@ def oep_get_data(schema, table, columns=[], conditions=[], order=''):
         logger.exception('Something went wrong during data retrieval from OEP: ')
 
     return pd.DataFrame(result.json())
+
+
+def oemof_nodes_from_excel(filename, header_lines=0):
+    """
+
+    Parameters
+    ----------
+    filename : :obj:`str`
+        Path to excel file
+
+    Returns
+    -------
+    :obj:`dict`
+        Imported nodes data
+    """
+    # excel file does not exist
+    if not filename or not os.path.isfile(filename):
+        logger.exception('Excel data file {} not found.'
+                         .format(filename))
+
+    xls = pd.ExcelFile(filename)
+
+    nodes_data = {'buses': xls.parse('buses', header=header_lines),
+                  'chp': xls.parse('chp', header=header_lines),
+                  'commodity_sources': xls.parse('commodity_sources', header=header_lines),
+                  'transformers': xls.parse('transformers', header=header_lines),
+                  'renewables': xls.parse('renewables', header=header_lines),
+                  'demand': xls.parse('demand', header=header_lines),
+                  'storages': xls.parse('storages', header=header_lines),
+                  'powerlines': xls.parse('powerlines', header=header_lines),
+                  'timeseries': xls.parse('time_series', header=header_lines)
+                  }
+
+    logger.info('Data from Excel file {} imported.'
+                .format(filename))
+
+    return nodes_data
