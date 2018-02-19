dab5d2a3cc96d9b26143fc09bdeda347a7c329ea
nesnoj <jonathan.amme@rl-institut.de>
2018-02-19 16:18:35 +0100

    adjust test script

--- windnode_abw/model/region/test.py
+++ windnode_abw/model/region/test.py
@@ -10,7 +10,7 @@ config.load_config('config_misc.cfg')
 from windnode_abw.tools.data import oep_get_data, oep_write_data
 from windnode_abw.tools.geo import convert_df_wkb_to_shapely, convert_df_shapely_to_wkb
 from windnode_abw.model.region.model import build_oemof_model
-from windnode_abw.model.region.tools import reduce_to_regions, region_graph
+from windnode_abw.model.region.tools import reduce_to_regions, region_graph, grid_graph
 
 import pandas as pd
 from shapely.geometry import LineString
@@ -35,16 +35,22 @@ from shapely.geometry import LineString
 # get HV grid
 buses = oep_get_data(schema='model_draft',
                      table='wn_abw_ego_pf_hv_bus',
-                     columns=['bus_id', 'hvmv_subst_id'])
+                     columns=['bus_id', 'hvmv_subst_id', 'region_bus', 'geom'])
+buses = convert_df_wkb_to_shapely(df=buses,
+                                  cols=['geom'])
 
 lines = oep_get_data(schema='model_draft',
                      table='wn_abw_ego_pf_hv_line',
                      columns=['line_id', 'bus0', 'bus1', 's_nom'])
 
+trafos = oep_get_data(schema='model_draft',
+                      table='wn_abw_ego_pf_hv_transformer',
+                      columns=['trafo_id', 'bus0', 'bus1', 's_nom', 'geom'])
+
 # get grid districts
 substations = oep_get_data(schema='model_draft',
                            table='wn_abw_ego_dp_hvmv_substation',
-                           columns=['subst_id', 'geom'])
+                           columns=['subst_id', 'otg_id', 'geom'])
 substations = convert_df_wkb_to_shapely(df=substations,
                                         cols=['geom'])
 substations.set_index('subst_id', inplace=True)
@@ -53,22 +59,29 @@ substations.set_index('subst_id', inplace=True)
 transport = reduce_to_regions(bus_data=buses,
                               line_data=lines)
 
-# prepare transport data for writig to OEP
-# join HV-MV substation ids drom buses on lines
-geoms = pd.concat([transport.join(substations,
-                                  on='hvmv_subst_id0')['geom'].rename('geom0'),
-                   transport.join(substations,
-                                  on='hvmv_subst_id1')['geom'].rename('geom1')],
-                  axis=1)
-def to_linestring(df):
-    return LineString([df['geom0'], df['geom1']])
-transport.loc[:,'geom'] = geoms.apply(to_linestring, axis=1)
+# ==== works only if substations are included in db table ====
+# # prepare transport data for writig to OEP
+# # join HV-MV substation ids drom buses on lines
+# geoms = pd.concat([transport.join(substations,
+#                                   on='hvmv_subst_id0')['geom'].rename('geom0'),
+#                    transport.join(substations,
+#                                   on='hvmv_subst_id1')['geom'].rename('geom1')],
+#                   axis=1)
+# def to_linestring(df):
+#     return LineString([df['geom0'], df['geom1']])
+# transport.loc[:,'geom'] = geoms.apply(to_linestring, axis=1)
 
 
 # oep_write_data(schema='model_draft',
 #                table='wn_abw_region_transport',
 #                data=convert_df_shapely_to_wkb(df=transport_data,
 #                                               cols=['geom']))
+
+graph = grid_graph(bus_data=buses,
+                   line_data=lines,
+                   subst_data=substations,
+                   trafo_data=trafos,
+                   draw=True)
 
 graph = region_graph(subst_data=substations,
                      line_data=transport,
