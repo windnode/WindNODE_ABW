674e0284021550c763200887804be8894a1deb17
nesnoj <jonathan.amme@rl-institut.de>
2018-02-26 16:35:59 +0100

    add Regio methods to obtain grouped genos

--- windnode_abw/model/__init__.py
+++ windnode_abw/model/__init__.py
@@ -61,6 +61,40 @@ class Region:
         """Returns region's conventional generators"""
         return self._geno_conv
 
+    @property
+    def geno_res_grouped(self):
+        """Returns grouped region's RES generators
+
+        Data is grouped by HV-MV-substation id and generation type,
+        count of generators and sum of nom. capacity is returned.
+
+        Returns
+        -------
+        :pandas:`pandas.DataFrame<dataframe>`
+            Grouped RES generators with MultiIndex
+        """
+        # access: e.g. df.loc[2303, 'gas']
+        # consider to reset index to convert index cols to regular cols
+
+        return self._geno_res.groupby(['subst_id', 'generation_type'])[
+            'capacity'].agg(['sum', 'count'])#.reset_index()
+
+    @property
+    def geno_conv_grouped(self):
+        """Returns grouped region's conventional generators
+
+        Data is grouped by HV-MV-substation id and fuel,
+        count of generators and sum of nom. capacity is returned.
+
+        Returns
+        -------
+        :pandas:`pandas.DataFrame<dataframe>`
+            Grouped conventional generators with MultiIndex
+        """
+
+        return self._geno_conv.groupby(['subst_id', 'fuel'])[
+            'capacity'].agg(['sum', 'count'])
+
     @classmethod
     def import_data(cls, **kwargs):
 
