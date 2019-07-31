3ae0397937d758182b0e78a2ae792dc8917b3b6d
nesnoj <jonathan.amme@rl-institut.de>
2019-07-31 18:05:53 +0200

    adapt Region class props and methods to new data structure

--- windnode_abw/model/__init__.py
+++ windnode_abw/model/__init__.py
@@ -5,8 +5,7 @@ import logging
 logger = logging.getLogger('windnode_abw')
 
 from windnode_abw.tools import config
-from windnode_abw.tools.data_io import \
-    oep_export_results, import_db_data
+from windnode_abw.tools.data_io import import_db_data
 from windnode_abw.model.region.tools import \
     prepare_feedin_timeseries, prepare_demand_timeseries
 
@@ -37,23 +36,23 @@ class Region:
     def __init__(self, **kwargs):
         self._name = 'ABW region'
 
+        self._muns = kwargs.get('muns', None)
         self._buses = kwargs.get('buses', None)
         self._lines = kwargs.get('lines', None)
         self._trafos = kwargs.get('trafos', None)
         self._subst = kwargs.get('subst', None)
-        self._geno_res = kwargs.get('geno_res', None)
-        self._geno_conv = kwargs.get('geno_conv', None)
-        self._geno_res_ts = kwargs.get('geno_res_ts', None)
-        self._demand_el = kwargs.get('demand_el', None)
-        self._demand_el_ts = kwargs.get('demand_el_ts', None)
+        self._generators = kwargs.get('generators', None)
         self._results_lines = kwargs.get('_results_lines', None)
 
-        self.muns = kwargs.get('muns', None)
-        self.demand_ts_init = kwargs.get('demand_ts_init', None)
-        self.demand_ts = None
-        self.feedin_ts_init = kwargs.get('feedin_ts_init', None)
-        self.feedin_ts = None
-        self.generators = kwargs.get('generators', None)
+        self._demand_ts_init = kwargs.get('demand_ts_init', None)
+        self._demand_ts = None
+        self._feedin_ts_init = kwargs.get('feedin_ts_init', None)
+        self._feedin_ts = None
+
+    @property
+    def muns(self):
+        """Returns region's municipalities"""
+        return self._muns
 
     @property
     def buses(self):
@@ -76,21 +75,9 @@ class Region:
         return self._subst
 
     @property
-    def geno_res(self, scenario='Status Quo'):
-        """Returns region's RES generators"""
-
-        return self._geno_res[self._geno_res['scenario'] == scenario]
-
-    @property
-    def geno_conv(self, scenario='Status Quo'):
-        """Returns region's conventional generators"""
-        return self._geno_conv[self._geno_conv['scenario'] == scenario]
-
-    @property
-    def geno_res_ts(self):
-        """Returns timeseries of region's RES generators"""
-
-        return self._geno_res_ts
+    def generators(self):
+        """Returns region's generators (renewable and conventional)"""
+        return self._generators
 
     @property
     def geno_res_grouped(self):
@@ -111,31 +98,12 @@ class Region:
             'capacity'].agg(['sum', 'count'])#.reset_index()
 
     @property
-    def geno_conv_grouped(self):
-        """Returns grouped region's conventional generators
+    def results_lines(self):
+        return self._results_lines
 
-        Data is grouped by HV-MV-substation/Grid District id and fuel,
-        count of generators and sum of nom. capacity is returned.
-
-        Returns
-        -------
-        :pandas:`pandas.DataFrame`
-            Grouped conventional generators with MultiIndex
-        """
-
-        return self.geno_conv.groupby(['subst_id', 'fuel'])[
-            'capacity'].agg(['sum', 'count'])
-
-    @property
-    def demand_el(self):
-        """Returns region's power demand per Grid District"""
-        return self._demand_el
-
-    @property
-    def demand_el_ts(self):
-        """Returns timeseries of region's demand"""
-
-        return self._demand_el_ts
+    @results_lines.setter
+    def results_lines(self, results_lines):
+        self._results_lines = results_lines
 
     @classmethod
     def import_data(cls, **kwargs):
@@ -148,20 +116,6 @@ class Region:
         region = cls(**kwargs)
 
         return region
-
-    @property
-    def results_lines(self):
-        return self._results_lines
-
-    @results_lines.setter
-    def results_lines(self, results_lines):
-        self._results_lines = results_lines
-
-    def export_results(self):
-        """Export simulation results"""
-
-        # export
-        oep_export_results(self)
 
     def dump_to_pkl(self, filename):
         """Dump Region to pickle"""
