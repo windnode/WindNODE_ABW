e4a59048a5f916d496681ac5666321e60fe40d90
nesnoj <jonathan.amme@rl-institut.de>
2019-09-20 10:06:53 +0200

    add initial params to the energy system

--- windnode_abw/scenarios/default.py
+++ windnode_abw/scenarios/default.py
@@ -89,8 +89,10 @@ def run_scenario(cfg):
     # add results to the energy system
     esys.results['main'] = outputlib.processing.results(om)
     esys.results['meta'] = outputlib.processing.meta_results(om)
-    # add om flows to allow access to Flow objects
-    esys.results['om_flows'] = list(om.flows.items())
+    # add initial params to the energy system
+    esys.results['params'] = outputlib.processing.parameter_as_dict(esys)
+    # # add om flows to allow access to Flow objects
+    # esys.results['om_flows'] = list(om.flows.items())
 
     # dump esys to file
     if cfg['dump_esys']:
