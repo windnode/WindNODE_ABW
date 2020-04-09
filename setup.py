5e20a4cb48fe7bf286c2b0d608ceb609ab4589a6
nesnoj <jonathan.amme@rl-institut.de>
2020-04-09 12:42:50 +0200

    rm obsolete base path creation

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
 from setuptools import find_packages, setup
 from setuptools.command.install import install
-import os
-
-BASEPATH='.WindNODE_ABW'
 
 
 class InstallSetup(install):
     def run(self):
-        #self.create_edisgo_path()
         install.run(self)
-
-    # @staticmethod
-    # def create_edisgo_path():
-    #     edisgo_path = os.path.join(os.path.expanduser('~'), BASEPATH)
-    #     data_path = os.path.join(edisgo_path, 'data')
-    #
-    #     if not os.path.isdir(edisgo_path):
-    #         os.mkdir(edisgo_path)
-    #     if not os.path.isdir(data_path):
-    #         os.mkdir(data_path)
 
 
 setup(
