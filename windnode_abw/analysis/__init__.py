aa6938e6bb799a0d2877d83066ef8b92e94f0f06
nesnoj <jonathan.amme@rl-institut.de>
2020-06-23 15:02:33 +0200

    move analysis steps to related module
    to allow import in other scripts

---
+++ windnode_abw/analysis/__init__.py
@@ -0,0 +1,107 @@
+import os
+
+import logging
+logger = logging.getLogger('windnode_abw')
+
+# load configs
+from windnode_abw.tools import config
+config.load_config('config_data.cfg')
+config.load_config('config_misc.cfg')
+
+from windnode_abw.tools.data_io import load_results
+from windnode_abw.model import Region
+
+from windnode_abw.analysis.tools import aggregate_flows, aggregate_parameters, flows_timexagsxtech, \
+    results_agsxlevelxtech, create_highlevel_results, results_tech
+
+
+def analysis(run_timestamp, scenarios):
+    """
+    Start analysis for single or multiple scenarios
+
+    Parameters
+    ----------
+    run_timestamp : :obj:`str`
+        Timestamp of run, e.g. '2020-06-17_125728_1month' (folder in
+        ~/.windnode_abw/results/ the scenarios are imported from.
+    scenarios : (:obj:`str`) OR (:obj:`list` of :obj:`str`)
+        Scenarios - select single or multiple scenarios manually (e.g.
+        'sq' or ['future', 'sq']) or use 'ALL' to analyze all scenarios
+        found in directory.
+
+    Returns
+    -------
+    :obj:`dict` of :obj:`dict` of :pandas:`pandas.DataFrame`
+        Dict with scenario names. Each entry holds a dict with a DataFrame
+        with converted columns.
+    """
+    if isinstance(scenarios, str):
+        scenarios = [scenarios]
+
+    # read available scenarios if 'ALL' requested
+    if scenarios == ['ALL']:
+        scenarios = [
+            file.split('.')[0]
+            for file in os.listdir(os.path.join(
+                config.get_data_root_dir(),
+                config.get('user_dirs',
+                           'results_dir'),
+                run_timestamp
+            )) if not file.startswith('.')
+        ]
+
+    logger.info(f'Analyzing {len(scenarios)} scenarios...')
+
+    results_scns = {}
+    timerange = None
+
+    for scn_id in scenarios:
+        logger.info(f'-> Analyzing scenario: {scn_id}...')
+
+        # load raw results
+        results_raw = load_results(timestamp=run_timestamp,
+                                   scenario=scn_id)
+
+        if results_raw is None:
+            logger.warning(f'Scenario {scn_id} not found, skipping...')
+        else:
+            results_scns[scn_id] = {}
+            # import region using cfg from results meta
+            cfg = results_raw['meta']['config']
+
+            # extract timerange and check consistency across multiple scenarios
+            if timerange is None:
+                timerange = [cfg.get('date_from'), cfg.get('date_to')]
+            else:
+                if timerange != [cfg.get('date_from'), cfg.get('date_to')]:
+                    msg = 'Simulation timeranges of different scenarios do ' \
+                          'not match!'
+                    logger.error(msg)
+                    raise ValueError(msg)
+
+            region = Region.import_data(cfg)
+
+            # Aggregate flow results along different dimensions (outdated, see #29)
+            #results = aggregate_flows(results_raw)
+
+            # Retrieve parameters from database and config file
+            parameters = aggregate_parameters(region, results_raw)
+            results_scns[scn_id]['parameters'] = parameters
+
+            # Flows extracted to dimension time, ags code, technology (and sometimes more dimensions)
+            flows_txaxt = flows_timexagsxtech(results_raw["flows"], region)
+            results_scns[scn_id]['flows_txaxt'] = flows_txaxt
+
+            # Aggregation of results to region level (dimensions: ags code (region) x technology)
+            results_axlxt = results_agsxlevelxtech(flows_txaxt, parameters, region)
+            results_scns[scn_id]['results_axlxt'] = results_axlxt
+
+            # Further aggregation and post-analysis calculations
+            results_t = results_tech(results_axlxt)
+            results_scns[scn_id]['results_t'] = results_t
+
+            # Aggregation to scalar result values
+            highlevel_results = create_highlevel_results(results_axlxt, results_t, flows_txaxt)
+            results_scns[scn_id]['highlevel_results'] = highlevel_results
+
+    return results_scns
--- windnode_abw/run_analysis.py
+++ windnode_abw/run_analysis.py
@@ -2,17 +2,12 @@
 from windnode_abw.tools.logger import setup_logger
 logger = setup_logger()
 
-import os
-
 # load configs
 from windnode_abw.tools import config
 config.load_config('config_data.cfg')
 config.load_config('config_misc.cfg')
-from windnode_abw.tools.data_io import load_results
-from windnode_abw.analysis.tools import aggregate_flows, aggregate_parameters, flows_timexagsxtech, \
-    results_agsxlevelxtech, create_highlevel_results, results_tech
-from windnode_abw.model import Region
-from windnode_abw.tools.draw import sample_plots
+
+from windnode_abw.analysis import analysis
 
 
 if __name__ == "__main__":
@@ -21,79 +16,13 @@ if __name__ == "__main__":
     # specify what to import (in path ~/.windnode_abw/)
     run_timestamp = '2020-06-17_125728_1month'
 
-    # select multiple scenarios manually or use ['ALL'] to analyze all
+    # select multiple scenarios manually or use 'ALL' to analyze all
     # scenarios found in directory
     #scenarios = ['future', 'sq']
     scenarios = ['ALL']
 
-    # read available scenarios if 'ALL' requested
-    if scenarios == ['ALL']:
-        scenarios = [
-            file.split('.')[0]
-            for file in os.listdir(os.path.join(
-                config.get_data_root_dir(),
-                config.get('user_dirs',
-                           'results_dir'),
-                run_timestamp
-            )) if not file.startswith('.')
-        ]
-
-    logger.info(f'Analyzing {len(scenarios)} scenarios...')
-
-    results_scns = {}
-    timerange = None
-
-    for scn_id in scenarios:
-        logger.info(f'Analyzing scenario: {scn_id}...')
-
-        # load raw results
-        results_raw = load_results(timestamp=run_timestamp,
-                                   scenario=scn_id)
-
-        if results_raw is None:
-            logger.warning(f'Scenario {scn_id} not found, skipping...')
-        else:
-            results_scns[scn_id] = {}
-            # import region using cfg from results meta
-            cfg = results_raw['meta']['config']
-
-            # extract timerange and check consistency across multiple scenarios
-            if timerange is None:
-                timerange = [cfg.get('date_from'), cfg.get('date_to')]
-            else:
-                if timerange != [cfg.get('date_from'), cfg.get('date_to')]:
-                    msg = 'Simulation timeranges of different scenarios do ' \
-                          'not match!'
-                    logger.error(msg)
-                    raise ValueError(msg)
-
-            region = Region.import_data(cfg)
-
-            # Aggregate flow results along different dimensions (outdated, see #29)
-            #results = aggregate_flows(results_raw)
-
-            # Retrieve parameters from database and config file
-            parameters = aggregate_parameters(region, results_raw)
-            results_scns[scn_id]['parameters'] = parameters
-
-            # Flows extracted to dimension time, ags code, technology (and sometimes more dimensions)
-            flows_txaxt = flows_timexagsxtech(results_raw["flows"], region)
-            results_scns[scn_id]['flows_txaxt'] = flows_txaxt
-
-            # Aggregation of results to region level (dimensions: ags code (region) x technology)
-            results_axlxt = results_agsxlevelxtech(flows_txaxt, parameters, region)
-            results_scns[scn_id]['results_axlxt'] = results_axlxt
-
-            # Further aggregation and post-analysis calculations
-            results_t = results_tech(results_axlxt)
-            results_scns[scn_id]['results_t'] = results_t
-
-            # Aggregation to scalar result values
-            highlevel_results = create_highlevel_results(results_axlxt, results_t, flows_txaxt)
-            results_scns[scn_id]['highlevel_results'] = highlevel_results
-
-            # sample_plots(region=region,
-            #              results=results)
+    results = analysis(run_timestamp=run_timestamp,
+                       scenarios=scenarios)
 
     logger.info('===== All done! =====')
 
